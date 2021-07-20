c96b3da60e6b23452e2187895d053fdcf2a0f75f
Koo82 <ilove209g@gmail.com>
2021-07-21 04:48:45 +0900

    bj_4991 completed with bfs and bit masking
    최단 경로를 구하는데 있어서 bfs가 최단 경로를 보장한다는
    것을 알지 못해서 꽤나 해맸던 문제.
    각 경로에서의 최단 거리의 단순 합이 최단 경로를 보장하지
    못하고, 경로의 순서에 따라서 달라진다.
    이에 따라 bit masking으로 맵전체를 표현하고 bfs를
    전개하면 답을 도출할 수 있다.
    
    아니면, bfs를 전개하여 각 도착점까지의 거리를 구하고
    permutation을 이용하여 푸는 방법도 있는 것 같은데
    풀이가 난해할 것 같아, 시도해 보지 않음
    기회가 되면 한번 풀어보는 것도 나쁘지 않을 듯.

--- bj_4991.py
+++ bj_4991.py
@@ -6,61 +6,74 @@ def log(message):
     if DEBUG:
         print(message)
 
+# "left, down, right, up" index for add
 idx, idy = [-1,0,1,0], [0,-1,0, 1]
-# w = sys.stdin.readline()
 
 def bfs(i, j, move, stage):
-
+    check = 1 # if check == 1, no solution
     deq = deque()
-    deq.append((i,j, move))
+    deq.append((i,j, move, stage)) # y_index, x_index, move count, stage for * bit masking
     visited[stage][i][j] = False
-    map_m[i][j] = '.'
-    while (deq and stage < target):
-        y, x, move = deq.popleft()
+
+    while deq:
+        y, x, move, stage = deq.popleft()
+        # log((y,x,move,stage))
+        # if all stage is visited, then bit masking is (1 <<target) -1, that is exit solution
+        if stage == (1 << target) -1 :
+            sys.stdout.write(f"{move}\n")
+            check = 0
+            break
+        # log(deq)
         for k in range(4):
             x_new = x + idx[k]
             y_new = y + idy[k]
+            # log((y_new, x_new, move, stage))
+
             if 0<=x_new <w and 0<= y_new<h and visited[stage][y_new][x_new]:
                 visited[stage][y_new][x_new] = False
-                if map_m[y_new][x_new] == '*':
-                    stage += 1
-                    visited[stage][y_new][x_new] = False
-                    map_m[y_new][x_new] = '.'
-                    deq.clear()
-                    deq.append((y_new, x_new, move+1))
-                    break
-                elif map_m[y_new][x_new] == '.':
-                    deq.append((y_new, x_new, move+1))
+                # -1 일 경우, 장애물이 있어서 전진 불가 skip처리
+                if map_m[y_new][x_new] == -1:
+                    continue
+                # stage이고, bit masking으로 방문하지 않은 곳을 확인하면 bit masking 추가하여 큐에 저장
+                if map_m[y_new][x_new] and not stage & (1 << (map_m[y_new][x_new]-1)):
+                    deq.append((y_new, x_new, move+1, stage | (1 << (map_m[y_new][x_new]-1))))
+                # 그렇지 않으면 ( stage가 아니거나, 이미 방문했다면 그냥 move +1 처리하고 큐에 추가
+                else:
+                    deq.append((y_new, x_new, move+1, stage))
 
-    return move, stage
+    if check:
+        sys.stdout.write(f"-1\n")
 
 while True:
     w, h = map(int, sys.stdin.readline().split())
     if w == 0 and h == 0:
         break
-# log(w)
+
     map_m = [list(sys.stdin.readline().rstrip()) for _ in range(h)]
     target = 0
     origin = (0,0)
+    # bfs 탐색을 위한 전처리, 'o'의 경우는 출발 위치 저장 그리고 0으로 setting, 별은 순서대로 1~10까지 번호
+    # 'x'는 방문이 어려우므로 -1처리 해준다
     for i in range(h):
         for j in range(w):
             if map_m[i][j] == 'o':
                 origin = (i, j)
+                map_m[i][j] = 0
             elif map_m[i][j] == '*':
                 target += 1
+                map_m[i][j] = target
+            elif map_m[i][j] == "x":
+                map_m[i][j] = -1
+            else:
+                map_m[i][j] = 0
 
     log(("origin", origin))
     log(('target number', target))
 
-    visited = [[[True] * w for _ in range(h)] for _ in range(target+1)]
+    visited = [[[True] * w for _ in range(h)] for _ in range(1 << (target+1))]
 
     log(("visited", visited))
+    log(("changed map is ", map_m))
+    bfs(origin[0], origin[1], 0, 0)
 
-    m, s = bfs(origin[0], origin[1], 0, 0)
-    if s == target:
-        sys.stdout.write(f"{m+1}\n")
-    elif target == 0:
-        sys.stdout.write(f"0\n")
-    else:
-        sys.stdout.write(f"-1\n")
 
